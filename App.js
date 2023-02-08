ea6b5280dd6c0715d5bd0b454ee8025c6e046eda
Khanh020903 <123916460+Khanh020903@users.noreply.github.com>
2023-02-08 19:56:53 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./ProductText/ProductList/Home";
+import Profile from "./ProductText/ProductList/Profile";
+import EditProfile from "./ProductText/ProductList/EditProfile";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
